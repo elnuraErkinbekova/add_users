5bec07bf091eabfa84cbd6ebd4a4552ce4aeb19c
elnuraErkinbekova <elnuraerkinbekova8@gmail.com>
2026-04-19 17:56:12 +0600

    UserList.jsx: add delete button to the list items

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,17 +1,19 @@
-import React from "react"
-import styles from './UserList.module.css'
-
+import React from "react";
+import styles from "./UserList.module.css";
 
 const UserList = (props) => {
-    return (
-        <div className={styles.users}>
-            <ul>
-                {props.users.map(user => {
-                    return <li key={user.id}>{user.name} ({user.age}) years</li>
-                })}
-            </ul>
-        </div>
-    )
-}
+	return (
+		<div className={styles.users}>
+			<ul>
+				{props.users.map((user) => (
+					<li key={user.id}>
+						{user.name} ({user.age}) years
+						<button onClick={() => props.onDelete(user.id)}>Delete</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
 
-export default UserList
+export default UserList;
