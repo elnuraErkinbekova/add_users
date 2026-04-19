78af83a725326d46bd06e962017d7fc494f46376
elnuraErkinbekova <elnuraerkinbekova8@gmail.com>
2026-04-19 18:17:54 +0600

    ErrorModal.jsx: change it back

--- src/components/ui/ErrorModal.jsx
+++ src/components/ui/ErrorModal.jsx
@@ -1,33 +1,32 @@
-import React, { Fragment } from "react";
-import ReactDOM from "react-dom";
-import Card from "./Card";
-import Button from "./Button";
-import classes from "./ErrorModal.module.css";
+import React, { Fragment } from 'react'
+import ReactDOM from 'react-dom';
+import Card from './Card'
+import Button from './Button'
+import classes from "./ErrorModal.module.css"
 
 const ErrorModal = (props) => {
-	return (
-		<>
-			{ReactDOM.createPortal(
-				<div className={classes.backdrop} onClick={props.onClose} />,
-				document.getElementById("backdrop-root"),
-			)}
-			{ReactDOM.createPortal(
-				<Card className={classes.modal}>
-					<header className={classes.header}>
-						<h2>{props.error}</h2>
-					</header>
-					<main className={classes.content}>
-						<p>{props.message}</p>
-					</main>
-					<footer className={classes.actions}>
-						<Button onClick={props.onConfirm}>YES</Button>
-						<Button onClick={props.onClose}>NO</Button>
-					</footer>
-				</Card>,
-				document.getElementById("modal-root"),
-			)}
-		</>
-	);
-};
+    return (
+        <>
+            {ReactDOM.createPortal(
+                <div className={classes.backdrop} onClick={props.onClose}/>,
+                document.getElementById('backdrop-root')
+            )}
+            {ReactDOM.createPortal(
+                <Card className={classes.modal}>
+                    <header className={classes.header}>
+                        <h2>{props.error}</h2>
+                    </header>
+                    <main className={classes.content}>
+                        <p>{props.message}</p>
+                    </main>
+                    <footer className={classes.actions}>
+                        <Button onClick={props.onClose}>OK</Button>
+                    </footer>
+                </Card>,
+                document.getElementById('modal-root')
+            )}
+        </>
+    )
+}
 
-export default ErrorModal;
+export default ErrorModal
